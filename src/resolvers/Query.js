f3858ce0a1c44b1d4fd48be5fb76ed9e7e966504
Ahad Bokhari <ahadbokhari@gmail.com>
2021-10-18 17:56:54 -0400

    base filter and pagination

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -6,6 +6,25 @@ function info(parent, args, context, info) {
     return `This is the API of a Hackernews Clone`
 }
 
+async function feed(parent, args, context, info) {
+    const where = args.filter
+      ? {
+        OR: [
+          { description: { contains: args.filter } },
+          { url: { contains: args.filter } },
+        ],
+      }
+      : {}
+  
+      const links = await context.prisma.link.findMany({
+        where,
+        skip: args.skip,
+        take: args.take,
+      })
+  
+    return links
+  }
+
 module.exports = {
     feed,
     info
