86fac66cd532b29d39075ee57765ec3743945bb3
Ahad Bokhari <ahadbokhari@gmail.com>
2021-10-20 17:29:46 -0400

    sort functionality

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -20,6 +20,7 @@ async function feed(parent, args, context, info) {
         where,
         skip: args.skip,
         take: args.take,
+        orderBy: args.orderBy
       })
   
     return links
